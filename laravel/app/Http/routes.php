75df351cd2698734e63477d8266ea63e986be1b1
fatalexe <ericseyden@yahoo.com>
2016-01-18 12:42:31 -0700

    working mysql

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -27,5 +29,7 @@ Route::get('/', function () {
 */
 
 Route::group(['middleware' => ['web']], function () {
-    //
+    Route::get('/hello',function () {
+        return view('welcome');
+    });
 });
