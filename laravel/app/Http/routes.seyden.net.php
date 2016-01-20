ee53a3c72fe446bdd2c6d73e5090c466bb1ab342
fatalexe <ericseyden@yahoo.com>
2016-01-19 20:45:04 -0700

    fix route

--- laravel/app/Http/routes.seyden.net.php
+++ laravel/app/Http/routes.seyden.net.php
@@ -3,6 +3,7 @@
 Route::get('/',function (){
     return view('welcome',['title'=>'Seyden Net']);
 });
-
-Route::get('login/twitter', 'Auth\SocialiteAuthController@redirectToProvider');
-Route::get('login/twitter/callback', 'Auth\SocialiteAuthController@handleProviderCallback');
+Route::group(['middleware' => 'web'], function() {
+    Route::get('login/twitter', 'Auth\SocialiteAuthController@redirectToProvider');
+    Route::get('login/twitter/callback', 'Auth\SocialiteAuthController@handleProviderCallback');
+});
