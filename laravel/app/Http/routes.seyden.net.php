265399b68fbe6f87b98c099ee134cd21e0dcbac0
fatalexe <ericseyden@yahoo.com>
2016-01-19 20:59:22 -0700

    maybe...

--- laravel/app/Http/routes.seyden.net.php
+++ laravel/app/Http/routes.seyden.net.php
@@ -4,6 +4,10 @@ Route::get('/',function (){
     return view('welcome',['title'=>'Seyden Net']);
 });
 Route::group(['middleware' => 'web'], function() {
+    Route::get('login/user',function(){
+        $user = Socialite::driver('github')->user();
+        dd($user);
+    });
     Route::get('login/twitter', 'Auth\SocialiteAuthController@redirectToProvider');
     Route::get('login/twitter/callback', 'Auth\SocialiteAuthController@handleProviderCallback');
 });
