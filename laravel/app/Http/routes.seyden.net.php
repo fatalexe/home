5f987957d2c0df5c18dce04378d4a8fc94cdb22f
fatalexe <ericseyden@yahoo.com>
2016-01-19 20:59:49 -0700

    maybe... test if we keep the twitter user

--- laravel/app/Http/routes.seyden.net.php
+++ laravel/app/Http/routes.seyden.net.php
@@ -4,8 +4,8 @@ Route::get('/',function (){
     return view('welcome',['title'=>'Seyden Net']);
 });
 Route::group(['middleware' => 'web'], function() {
-    Route::get('login/user',function(){
-        $user = Socialite::driver('github')->user();
+    Route::get('login/twitter/user',function(){
+        $user = Socialite::driver('twitter')->user();
         dd($user);
     });
     Route::get('login/twitter', 'Auth\SocialiteAuthController@redirectToProvider');
